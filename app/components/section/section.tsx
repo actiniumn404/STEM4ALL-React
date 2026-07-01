914fd56ab38fb41a17d0c52db8778f37a5e8edd3
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2026-06-30 22:14:20 -0700

    Scroll reveal (thanks Eesha)

--- app/components/section/section.tsx
+++ app/components/section/section.tsx
@@ -1,13 +1,18 @@
 import "./section.scss"
+import FadeInSection from "~/components/FadeIn";
 
 export default function Section({section_id, section_content_id, title, children}){
     return <div className="section" id={section_id}>
-        <div className="section-header">
-            <h1 className="section-header-text">{title}</h1>
-        </div>
+        <FadeInSection>
+            <div className="section-header">
+                <h1 className="section-header-text">{title}</h1>
+            </div>
+        </FadeInSection>
+        <FadeInSection>
+            <div className="section-content" id={section_content_id}>
+                {children}
+            </div>
+        </FadeInSection>
 
-        <div className="section-content" id={section_content_id}>
-            {children}
-        </div>
     </div>
 }
